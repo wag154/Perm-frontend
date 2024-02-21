7b6da6c9017ffb65968494ff0ab504bd3c7d2a90
Jack <jackkevinperkins03@gmail.com>
2024-02-21 19:45:49 +0000

    ugly 1.2, added perm route and open and closeable sidenav

--- src/layout/SideBar/index.jsx
+++ src/layout/SideBar/index.jsx
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 import styles from "./index.module.css"
 import { NavLink, Outlet } from 'react-router-dom';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import homeImg from "./home.png";
+import containerImg from "./carton-box.png";
 
 export default function SideBar() {
   const [closed, setClosed] = useState(false);
-  console.log(closed)
   return (
     <>
     <nav className={`${ closed ?styles["SideNavClosed"] : styles["SideNavOpen"]} `}>
         <ul className={styles["HamburgerContainer"]}>
           <RxHamburgerMenu className ={styles["MenuButton"]} onClick={()=>{setClosed(!closed)}} />
+
         </ul>
         <ul>
-          no
+          <NavLink to = "/worked" className={styles["HomeContainer"]}><img src = {homeImg} className={styles["HomeIconDisplay"]} /> {closed ? null : "Dashboard"} </NavLink>
+        </ul>
+        <ul>
+          <NavLink to = "" className={styles["ContentContainer"]}><img src = {containerImg} className={styles["ContainerIconDisplay"]}/> {closed ? null : "Resources"}</NavLink>
         </ul>
     </nav>
     <Outlet />
